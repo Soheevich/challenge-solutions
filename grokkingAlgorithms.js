eea1c877b7ee42eb542838e453d13025b308efb1
Andrei F <soheevich@gmail.com>
2018-06-02 14:19:36 +0300

    Add Recursive sum

--- grokkingAlgorithms.js
+++ grokkingAlgorithms.js
@@ -62,3 +62,13 @@ const selectionSort = (arr) => {
 
   return sortedArray;
 };
+
+
+// Recursive sum
+
+const sum = (array) => {
+  if (array.length === 1) return array[0];
+  else return array[0] + sum(array.slice(1));
+};
+
+
